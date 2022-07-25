216856fadd5ef23407271158a889ba5a270e5691
Akshat9080 <81543625+Akshat9080@users.noreply.github.com>
2022-07-25 18:04:56 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,24 @@
+function bg_black() {
+    document.getElementById("data1").style.backgroundColor = "black";
+    document.getElementById("t1").style.borderColor = "white";
+    document.getElementById("t1").style.backgroundColor = "black";
+    document.getElementById("t1").style.color = "white";
+  }
+  function bg_green() {
+      document.getElementById("data1").style.backgroundColor = "green";
+      document.getElementById("t1").style.borderColor = "white";
+      document.getElementById("t1").style.backgroundColor = "green";
+      document.getElementById("t1").style.color = "white";
+  }
+  function bg_yellow() {
+      document.getElementById("data1").style.backgroundColor = "yellow";
+      document.getElementById("t1").style.borderColor = "black";
+      document.getElementById("t1").style.backgroundColor = "yellow";
+      document.getElementById("t1").style.color = "black";
+  }
+  function bg_blue() {
+      document.getElementById("data1").style.backgroundColor = "blue";
+      document.getElementById("t1").style.borderColor = "white";
+      document.getElementById("t1").style.backgroundColor = "blue";
+      document.getElementById("t1").style.color = "white";
+  }
